9acd8e1868dff7a9db963c9116eb22884d75dc3f
shaif-ali-javad <shaifalijavad60@gmail.com>
2023-11-10 07:35:49 +0530

    testing

--- src/component/home/Services/img-component/img.js
+++ src/component/home/Services/img-component/img.js
@@ -3,16 +3,18 @@ import "./img.css";
 import name from "../image/20512813.jpg";
 
 const Img = () => (
-  <Parallax
-    className="img-main"
-    bgImage={name}
-    bgImageAlt="the cat"
-    strength={100}
-  >
-    <div className="content-main">
-      <span className="img-main-txt">Onetech</span>
-    </div>
-  </Parallax>
+  <div className="img-main-over">
+    <Parallax
+      className="img-main"
+      bgImage={name}
+      bgImageAlt="the cat"
+      strength={100}
+    >
+      <div className="content-main">
+        <span className="img-main-txt">Onetech</span>
+      </div>
+    </Parallax>
+  </div>
 );
 
 export default Img;
