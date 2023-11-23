10027bac397da8d078aba0aa4e111d4255d87f21
shaif-ali-javad <shaifalijavad60@gmail.com>
2023-11-24 01:50:44 +0530

    testing

--- src/component/home/partners/logo-carousel.js
+++ src/component/home/partners/logo-carousel.js
@@ -22,7 +22,7 @@ export default class LogoCarousel extends Component {
       dots: true,
       infinite: true,
       speed: 2000,
-      slidesToShow: 3,
+      slidesToShow: 2,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
